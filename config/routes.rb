fec59fbf05db33564c3a257811bac4eb07e36674
ryan715 <yxr.306789@gmail.com>
2017-08-08 23:57:07 +0800

    login view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'home#welcome'
+  
   resources :commodities
   resources :commodity_types
   resources :warehouses
   devise_for :users
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
